590a3927e29995300a42ebb21a21c880692a4b41
Ahmad Piracha <ahmadpiracha3@gmail.com>
2023-08-12 21:14:34 +0500

    commited changes

--- Basics/functions.ts
+++ Basics/functions.ts
@@ -1,5 +1,6 @@
-function addTwo (num:number) {
+function addTwo (num:number):number {
     return num+2;
+    // return "ahmad";
 }
 
 addTwo(6);
@@ -13,5 +14,32 @@ let loginUser = (name:string,email:string,isPaid:boolean=false) =>{}
 
 loginUser("ahmad","a@a.com")
 getUpper("ahmad");
+
+
+// function getValue(myVal:number):boolean{
+//     if(myVal>10){
+//         return true;
+//     }
+//     return "200 OK";
+// }
+
+const getHello = (s:string):string => {
+return "";
+}
+
+const heros = ["ahmad","ali","khan"];
+// const heros = [1,2,3]
+heros.map((hero:string) => {
+    return `${hero} is a hero`
+})
+
+
+function consoleError(err:string):void{
+    console.log(err);
+}
+
+function handleError(err:string):never{
+    throw new Error(err);
+}
 
 export {}
